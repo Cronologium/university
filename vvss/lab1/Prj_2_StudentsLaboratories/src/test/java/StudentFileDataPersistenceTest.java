deadb541af0f04eb6c0dfd2f835792c4685adb28
Tudor Varan <Cronologium@github.com>
2018-03-27 15:56:45 +0300

    Added JUnit Tests

---
+++ vvss/lab1/Prj_2_StudentsLaboratories/src/test/java/StudentFileDataPersistenceTest.java
@@ -0,0 +1,27 @@
+import controller.LaboratoriesController;
+import model.Student;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StudentFileDataPersistenceTest {
+
+    private LaboratoriesController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new LaboratoriesController("test_file.txt", null);
+    }
+
+    @Test
+    void testAddValidStudent() {
+        assertEquals(true, controller.saveStudent(new Student("test2021", "Test Test", 456)));
+    }
+
+    @Test
+    void testAddInvalidStudent() {
+        assertEquals(false, controller.saveStudent(new Student("ajahdgasjdhasgda", "asdjaskjdhask", 23874623)));
+    }
+}
