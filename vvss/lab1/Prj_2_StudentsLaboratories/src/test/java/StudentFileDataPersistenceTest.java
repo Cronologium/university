865db91e0491103c7ed7b3cca14d8b6c75fa67db
Tudor Varan <Cronologium@github.com>
2018-04-01 19:08:52 +0300

    Added black box tests for problem

--- vvss/lab1/Prj_2_StudentsLaboratories/src/test/java/StudentFileDataPersistenceTest.java
+++ vvss/lab1/Prj_2_StudentsLaboratories/src/test/java/StudentFileDataPersistenceTest.java
@@ -14,13 +14,71 @@ public class StudentFileDataPersistenceTest {
         controller = new LaboratoriesController("test_file.txt", null);
     }
 
-    @Test
+    @Test // Test case #1
     public void testAddValidStudent() {
-        assertEquals(true, controller.saveStudent(new Student("test2021", "Test Test", 456)));
+        assertEquals(true, controller.saveStudent(new Student("test1234", "test test", 123)));
     }
 
-    @Test
+    @Test // Test case #2
+    public void testAddStudentWithGroupAtInferiorBoundary() {
+        assertEquals(false, controller.saveStudent(new Student("test1234", "test test", 0)));
+    }
+
+    @Test // Test case #3
+    public void testAddStudentWithGroupAtSuperiorBoundary() {
+        assertEquals(false, controller.saveStudent(new Student("test1234", "test test", 1000)));
+    }
+
+    @Test // Test case #4
+    public void testAddStudentWithGroupSmallerThanInferiorBoundary() {
+        assertEquals(false, controller.saveStudent(new Student("test1234", "test test", -1)));
+
+    }
+
+    @Test // Test case #5
+    public void testAddStudentWithGroupBiggerThanSuperiorBoundary() {
+        assertEquals(false, controller.saveStudent(new Student("test1234", "test test", 1001)));
+    }
+
+    @Test // Test case #6
+    public void testAddStudentWithGroupNearSuperiorBoundary() {
+        assertEquals(true, controller.saveStudent(new Student("test1234", "test test", 999)));
+    }
+
+    @Test // Test case #7
+    public void testAddStudentWithGroupNearInferiorBoundary() {
+        assertEquals(true, controller.saveStudent(new Student("test1234", "test test", 1)));
+    }
+
+    @Test // Test case #8
+    public void testAddStudentWithRegHavingLessLetters() {
+        assertEquals(false, controller.saveStudent(new Student("tes1234", "test test", 123)));
+    }
+
+    @Test // Test case #9
+    public void testAddStudentWithRegHavingLessDigits() {
+        assertEquals(false, controller.saveStudent(new Student("test123", "test test", 123)));
+    }
+
+    @Test // Test case #10
+    public void testAddStudentWithNameHavingLessWord() {
+        assertEquals(false, controller.saveStudent(new Student("test1234", "test", 123)));
+    }
+
+    @Test // Test case #11
+    public void testAddStudentWithNameHavingMoreWords() {
+        assertEquals(false, controller.saveStudent(new Student("test1234", "test test test", 123)));
+    }
+
+    @Test // Test case #12
+    public void testAddStudentWithNameHavingDigits() {
+        assertEquals(false, controller.saveStudent(new Student("test1234", "test test1", 123)));
+    }
+
+    @Test // Not really a documented test case, leftover from laboratory work
     public void testAddInvalidStudent() {
         assertEquals(false, controller.saveStudent(new Student("ajahdgasjdhasgda", "asdjaskjdhask", 23874623)));
     }
+
+
 }
